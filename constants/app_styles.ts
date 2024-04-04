fcc18ce29fff615fd539b8a78ae086fc2552ba91
AkritiSaraogi <akritisaraogi@gmail.com>
2024-04-04 10:41:47 +0530

    iOS Config

--- constants/app_styles.ts
+++ constants/app_styles.ts
@@ -21,32 +21,6 @@ export const styles = StyleSheet.create({
         color: 'red',
         marginTop: 10,
     },
-    bottomSheetContainer: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        borderRadius: 10
-    },
-    bottomSheetHeader: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        padding: 10,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20
-    },
-    closeIcon: {
-        fontSize: 20,
-    },
-    bottomSheetTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    bottomSheetContent: {
-        backgroundColor: '#fff',
-        padding: 20,
-        height: '60%',
-    },
     textstyle: {
         color: 'blue',
         fontSize: 16
